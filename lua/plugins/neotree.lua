79f12aeba20c0686e70a00f264cd652cbb280a1f
rahelv <89937339+rahelv@users.noreply.github.com>
2024-04-16 19:38:59 +0200

    open pdf etc. in system viewer

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,15 +1,29 @@
 -- filetree navigation
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
+    'nvim-neo-tree/neo-tree.nvim',
+    branch = 'v3.x',
     dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+        'nvim-lua/plenary.nvim',
+        'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+        'MunifTanjim/nui.nvim',
+        -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
     },
     keys = {
-        { "<leader>e", "<cmd>Neotree toggle<CR>", desc = "Toggle file explorer (root dir)"},
-        { "<leader>E", "<cmd>Neotree reveal<CR>", desc = "Toggle file explorer (cwd)"},
-    }
+        { '<leader>e', '<cmd>Neotree toggle<CR>', desc = 'Toggle file explorer (root dir)' },
+        { '<leader>E', '<cmd>Neotree reveal<CR>', desc = 'Toggle file explorer (cwd)' },
+    },
+    opts = {
+        window = {
+            mappings = {
+                ['o'] = 'system_open',
+            },
+        },
+        commands = {
+            system_open = function(state)
+                local node = state.tree:get_node()
+                local path = node:get_id()
+                vim.fn.jobstart({ 'xdg-open', path }, { detach = true })
+            end,
+        },
+    },
 }
